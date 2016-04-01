3c904a6d7a1d2b6ad49baa6d9da1b67b974f7ad4
michael <michael.vang.1985@gmail.com>
2016-04-01 11:26:50 -0700

    add question display to question hbs

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   answer: DS.attr(),
-  notes: DS.attr(),
   question: DS.belongsTo('question', {async: true})
 });
