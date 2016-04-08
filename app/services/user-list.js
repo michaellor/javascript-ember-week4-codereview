bbbea4c175f1819dc5f5e237ce4c9992d224f709
michael <michael.vang.1985@gmail.com>
2016-04-08 11:37:06 -0700

    add forloop to userlist service

--- app/services/user-list.js
+++ app/services/user-list.js
@@ -4,6 +4,15 @@ export default Ember.Service.extend({
   answers: [],
 
   add(answer) {
-    this.get('answers').pushObject(answer);
+    console.log("1");
+    length: this.get('answers').length;
+    for(var i=0; i < length; i++) {
+      console.log("length", length);
+      if(answer === (this.get('answers')[i])) {
+        console.log("already on list");
+      } else {
+        this.get('answers').pushObject(answer);
+      }
+    }
   }
 });
