3109eaf8e4f822de9503195272111fb893d66e67
michael <michael.vang.1985@gmail.com>
2016-04-08 09:50:12 -0700

    add conditional if statement to count helper

--- app/helpers/cold-question.js
+++ app/helpers/cold-question.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 
 export function coldQuestion(params) {
   var question = params[0];
-  console.log(params[0])
 
   if(question.get('answers').get('length') < 1) {
-    return Ember.String.htmlSafe(' <span class="glyphicon glyphicon-chevron-down color-blue"></span>')
+    return Ember.String.htmlSafe(' <span class="glyphicon glyphicon-question-sign color-blue"></span>')
   } else if(question.get('answers').get('length') > 3) {
     return Ember.String.htmlSafe(' <span class="glyphicon glyphicon-fire color-red"></span>')
   }
--- app/helpers/count-answers.js
+++ app/helpers/count-answers.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
-export function countAnswers(params/*, hash*/) {
-  return params;
+export function countAnswers(params) {
+  var question = params[0];
+  console.log(question);
+  var answers = question.get('answers').get('length');
+
+  if(answers === 1) {
+    return Ember.String.htmlSafe( '  <span id="small-font">('+ answers +' answer)</span>');
+  }
+  return Ember.String.htmlSafe( '  <span id="small-font">('+ answers +' answers)</span>');
 }
 
 export default Ember.Helper.helper(countAnswers);
